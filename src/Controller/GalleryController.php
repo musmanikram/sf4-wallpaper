907d8e0a7d5da9b296c988cc610a8393e1d60ba3
Usman <usman.ikram@codeinformatics.com>
2018-02-10 16:40:45 +0500

    added pagination

---
+++ src/Controller/GalleryController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usman
+ * Date: 31/01/2018
+ * Time: 3:34 PM
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class GalleryController extends Controller {
+
+	/**
+	 * @Route("/gallery", name="gallery")
+	 * @param Request $request
+	 */
+
+	public function index(Request $request)
+	{
+		$images = [
+			"1-nature.jpg",
+			"2-nature.jpg",
+			"3-nature.jpg",
+			"4-nature.jpg",
+			"5-nature.jpg",
+			"6-nature.jpg",
+			"7-nature.jpg",
+			"11-nature.jpg",
+			"12-nature.jpg",
+			"13-nature.jpg",
+			"14-nature.jpg",
+			"15-nature.jpg",
+			"16-nature.jpg"
+		];
+
+		$paginator  = $this->get('knp_paginator');
+		$pagination = $paginator->paginate(
+			$images,
+			$request->query->getInt('page', 1)/*page number*/,
+			4/*limit per page*/
+		);
+
+		return $this->render("gallery/index.html.twig",[
+			'images' => $pagination
+		]);
+	}
+
+}
