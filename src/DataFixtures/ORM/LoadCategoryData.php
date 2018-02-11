3036d5197f10c198c6a08660956200eedafe8104
Usman <usman.ikram@codeinformatics.com>
2018-02-12 02:13:23 +0500

    updated fixtures with order and removed images

--- src/DataFixtures/ORM/LoadCategoryData.php
+++ src/DataFixtures/ORM/LoadCategoryData.php
@@ -2,36 +2,38 @@
 
 namespace App\DataFixtures\ORM;
 
+use App\Entity\Category;
 use App\Entity\Wallpaper;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadWallpaperData extends Fixture implements OrderedFixtureInterface
+class LoadCategoryData extends Fixture implements OrderedFixtureInterface
 {
 	public function load(ObjectManager $manager): void
 	{
 
-		$wallpaper = new Wallpaper();
-		$wallpaper->setFilename('1-nature.jpg');
-		$wallpaper->setSlug('1-nature');
-		$wallpaper->setDescription("");
-		$wallpaper->setWidth(1920);
-		$wallpaper->setHeight(1080);
-		$wallpaper->setCategory($this->getReference('category.abstract'));
-		$manager->persist($wallpaper);
+		$category = new Category();
+		$category->setName('Abstract');
+		$this->addReference('category.abstract', $category);
+		$manager->persist($category);
 
 
 		$manager->flush();
 	}
-
 
 	/**
 	 * Get the order of this fixture
 	 *
 	 * @return integer
 	 */
+
 	public function getOrder() {
-		return 200;
+		// TODO: Implement getOrder() method.
+
+		return 100;
 	}
+
+
 }
